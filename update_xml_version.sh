e96caba0fb7c3cd6f6e2863c31d6ad4d0118961b
Narch <zeropior@proton.me>
2026-09-22 20:19:59 -0300

    remove bash scripts

--- update_xml_version.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-VERSION=$1
-# strip v from v3.5.0
-VERSION=${VERSION#v}
-sed -i "s|<versionNumber>.*</versionNumber>|<versionNumber>${VERSION}</versionNumber>|" loader/Mobile-app.xml
-sed -i "s|<versionNumber>.*</versionNumber>|<versionNumber>${VERSION}</versionNumber>|" loader/Desktop-app.xml
